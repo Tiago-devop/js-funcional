3da6dc9e25176a517fabf3c7353d0a5b4c80c3c2
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-09-25 13:34:04 -0300

    consumindo api github p/ obter repositorios

--- rxjs/operadores_1.js
+++ rxjs/operadores_1.js
@@ -18,6 +18,6 @@ of(1, 2, 'tiago', false, 'último')
         map(v => v[0]),
         map(v => `A letra encontrada foi: ${v}`)
     )
-    .subscribe(function(valor) {
+    .subscribe(function (valor) {
         console.log(valor)
     })
