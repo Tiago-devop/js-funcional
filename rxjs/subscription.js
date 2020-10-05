0080381fb38a08ea75cb1748de18487baed2b94c
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-05 13:34:09 -0300

    usando subscription

---
+++ rxjs/subscription.js
@@ -0,0 +1,22 @@
+const { VirtualTimeScheduler, Subscription } = require("rxjs");
+
+const { timer } = require('rxjs')
+
+const obs = timer(3000, 500)
+
+const sub1 = obs.subscribe(num => {
+    console.log(`#1 Gerou o número ${num}`)
+})
+
+const sub2 = obs.subscribe(num => {
+    console.log(`#2 Gerou o número ${num}`)
+})
+
+const sub = new Subscription()
+sub.add(sub1)
+sub.add(sub2)
+
+setTimeout(() => {
+    sub.unsubscribe()
+    // sub2.unsubscribe()
+}, 5000)
